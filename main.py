b1148fedc719f0716fb90abab8ad9208d05de97c
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-17 09:02:49 -0600

    Attempt for dynamic

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, url_for
+import os
+
+video_path = []
+for root, dirs, files in os.walk('static/videos'):
+    for file in files:
+        video_path.append("videos/" + file)
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -6,7 +12,7 @@ app.static_folder = 'static'
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("videoGallery.html", videoPaths=video_path)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
