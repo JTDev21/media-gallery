b26bc386f11ae08a3a080702d076de6cf8fe1fdd
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-17 13:31:36 -0600

    Media Stuff

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ for root, dirs, files in os.walk('static/videos'):
 app = Flask(__name__)
 app.static_folder = 'static'
 
-
 @app.route("/")
+@app.route("/home")
 def home():
+    return render_template("home.html")
+
+@app.route("/playlist")
+def videoPlaylist():
     return render_template("videoGallery.html", videoPaths=video_path)
+
+@app.route("/photoGallery")
+def photoGallery():
+    return render_template("photoGallery.html")
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
