a5c25b220b677db0c3d58f03b31c06b685436f68
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-18 00:56:02 -0600

    Image gallery

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ for root, dirs, files in os.walk('static/videos'):
     for file in files:
         video_path.append("videos/" + file)
 
+image_path = []
+for root, dirs, files in os.walk('static/images'):
+    for file in files:
+        image_path.append("images/" + file)
+
 app = Flask(__name__)
 app.static_folder = 'static'
 
@@ -20,7 +25,7 @@ def videoPlaylist():
 
 @app.route("/photoGallery")
 def photoGallery():
-    return render_template("photoGallery.html")
+    return render_template("photoGallery.html", imagePaths=image_path)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
