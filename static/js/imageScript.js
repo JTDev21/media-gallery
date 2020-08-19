21b08d7a324e37ddecdcd5b7699bb96fccce3a72
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-19 00:42:38 -0600

    Expand Image

--- static/js/imageScript.js
+++ static/js/imageScript.js
@@ -1,18 +1,27 @@
 document.getElementById("c-image-display").style.display = "none";
 var img_container = document.getElementById("c-image-display");
-var containerToBlur = document.getElementById('masonary-gallery');
+var gallery_containerToBlur = document.getElementById('masonary-gallery');
+var navbar_containerToBlur = document.getElementById('my-navbar');
+var noScrollBar = document.getElementById('body');
 
 function expandImg(_img) {
     if (img_container.style.display === "none") {
         // Enable Blur
-        containerToBlur.setAttribute('class', containerToBlur.getAttribute('class') + ' blurBackground');
+        noScrollBar.setAttribute('class', 'no-scroll');
+        gallery_containerToBlur.setAttribute('class', gallery_containerToBlur.getAttribute('class') + ' blurBackground');
+        navbar_containerToBlur.setAttribute('class', navbar_containerToBlur.getAttribute('class') + ' blurBackground');
 
         img_container.style.display = "block";
     } 
     else {
+        noScrollBar.classList.remove('no-scroll');
+
         // Disable Blur
-        containerToBlur.classList.remove('blurBackground')
-        containerToBlur.setAttribute('class', containerToBlur.getAttribute('class'));
+        gallery_containerToBlur.classList.remove('blurBackground');
+        gallery_containerToBlur.setAttribute('class', gallery_containerToBlur.getAttribute('class'));
+
+        navbar_containerToBlur.classList.remove('blurBackground');
+        navbar_containerToBlur.setAttribute('class', navbar_containerToBlur.getAttribute('class'));
 
         img_container.style.display = "none";
     }
