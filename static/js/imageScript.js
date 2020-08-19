588b0ab26bf400d18027c547a3b13b576092caff
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-18 19:56:44 -0600

    expand image

--- static/js/imageScript.js
+++ static/js/imageScript.js
@@ -1,8 +1,27 @@
-var img = document.getElementsByTagName('img');
+document.getElementById("c-image-display").style.display = "none";
+var img_container = document.getElementById("c-image-display");
+var containerToBlur = document.getElementById('masonary-gallery');
 
+function expandImg(_img) {
+    if (img_container.style.display === "none") {
+        // Enable Blur
+        containerToBlur.setAttribute('class', containerToBlur.getAttribute('class') + ' blurBackground');
 
+        img_container.style.display = "block";
+        var img = document.getElementById("enlarge-display");
+        img.src = "https://wallpapercave.com/wp/wp2551790.jpg";
+    } 
+    else {
+        // Disable Blur
+        containerToBlur.classList.remove('blurBackground')
+        containerToBlur.setAttribute('class', containerToBlur.getAttribute('class'));
 
-// console.log(img.length);
-// for (var i = 0; i < img.length;i++) {
-//     console.log(img[i].src);
-// }
+        img_container.style.display = "none";
+    }
+}
+
+img_container.onclick = function() {
+    if (img_container.style.display === "block") {
+        expandImg(this);
+    }
+};
