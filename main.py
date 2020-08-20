40b3e8b55421bff4e061036fcd07e444d810060c
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-19 23:45:12 -0600

    video in masonary gallery

--- main.py
+++ main.py
@@ -2,19 +2,38 @@ from flask import Flask, render_template, url_for
 from flask_jsglue import JSGlue
 import os
 
+
+
 video_path = []
 for root, dirs, files in os.walk('static/videos'):
     for file in files:
         video_path.append("videos/" + file)
 
-image_path = []
-for root, dirs, files in os.walk('static/images'):
+media_filepaths = []
+media_ext_type = []
+video_ext = ['m4v', 'avi', 'mpg', 'mp4']
+img_ext = ['jpg', 'gif', 'bmp', 'png']
+
+for root, dirs, files in os.walk('static/media_files'):
     for file in files:
-        image_path.append("images/" + file)
+        media_filepaths.append("media_files/" + file)
+        media_ext = (file.split('.'))[-1]
+        for ext in video_ext:
+            if (media_ext == ext):
+                media_ext_type.append('video')
+        for ext in img_ext:
+            if (media_ext == ext):
+                media_ext_type.append('image')
+
+
+# print("media_filepaths", len(media_filepaths))
+# print("media_ext_type", len(media_ext_type))
 
 app = Flask(__name__)
 app.static_folder = 'static'
 _jsglue = JSGlue(app)
+
+
 
 @app.route("/")
 @app.route("/home")
@@ -23,11 +42,11 @@ def home():
 
 @app.route("/playlist")
 def videoPlaylist():
-    return render_template("videoGallery.html", videoPaths=video_path)
+    return render_template("youtubeVideos.html", videoPaths=video_path)
 
-@app.route("/photoGallery")
+@app.route("/masonaryGallery.html")
 def photoGallery():
-    return render_template("photoGallery.html", imagePaths=image_path, jsglue=_jsglue)
+    return render_template("masonaryGallery.html", jsglue=_jsglue, mediaFiles=media_filepaths, mediaEXT=media_ext_type)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
--- static/js/imageScript.js
+++ static/js/imageScript.js
@@ -1,17 +1,25 @@
-document.getElementById("c-image-display").style.display = "none";
-var img_container = document.getElementById("c-image-display");
+document.getElementById("c-media").style.display = "none";
+var media_container = document.getElementById("c-media");
+
 var gallery_containerToBlur = document.getElementById('masonary-gallery');
 var navbar_containerToBlur = document.getElementById('my-navbar');
 var noScrollBar = document.getElementById('body');
 
-function expandImg(_img) {
-    if (img_container.style.display === "none") {
+var img_div = document.getElementById("c-image-display");
+var video_div = document.getElementById("c-video-display");
+
+function viewer(_img) {
+    if (media_container.style.display === "none") {
         // Enable Blur
         noScrollBar.setAttribute('class', 'no-scroll');
         gallery_containerToBlur.setAttribute('class', gallery_containerToBlur.getAttribute('class') + ' blurBackground');
         navbar_containerToBlur.setAttribute('class', navbar_containerToBlur.getAttribute('class') + ' blurBackground');
 
-        img_container.style.display = "block";
+        media_container.style.display = "block";
+        video_div.style.display = "block";
+        img_div.style.display = "block";
+
+        viewFile(_img);
     } 
     else {
         noScrollBar.classList.remove('no-scroll');
@@ -23,24 +31,86 @@ function expandImg(_img) {
         navbar_containerToBlur.classList.remove('blurBackground');
         navbar_containerToBlur.setAttribute('class', navbar_containerToBlur.getAttribute('class'));
 
-        img_container.style.display = "none";
+        media_container.style.display = "none";
+        video_div.style.display = "none";
+        img_div.style.display = "none";
     }
 }
 
-function displayImg(_img) {
-    var filepath = _img.src;
-    var splitPath = filepath.split('/');
-    var findIndex = splitPath.indexOf('images');
+function isImage(ext) {
+    switch (ext.toLowerCase()) {
+        case 'jpg':
+        case 'gif':
+        case 'bmp':
+        case 'png':
+        //etc
+        return true;
+    }
+    return false;
+}
+
+function isVideo(ext) {
+    switch (ext.toLowerCase()) {
+        case 'm4v':
+        case 'avi':
+        case 'mpg':
+        case 'mp4':
+        // etc
+        return true;
+    }
+    return false;
+}
+
+function viewFile(file) {
+    var objectPath = file.src;
+
+    var img_filename = objectPath.split('.');
+    var video_filename = objectPath.split('.');
+    
+    var img_ext = String(video_filename[img_filename.length - 1]);
+    var video_ext = String(video_filename[video_filename - 1]);
+
+    var splitPath = objectPath.split('/');
+    var findIndex = splitPath.indexOf('media_files');
     var splitPath = splitPath.slice(findIndex, splitPath.length);
-    var reconstruct = splitPath.join('/')
+    var reconstruct = splitPath.join('/');
 
-    var img = document.getElementById("enlarge-display");
-    img.src = Flask.url_for('static', {'filename': reconstruct});
+    // console.log(reconstruct);
+    if (isImage(img_ext)) {
+        video_div.style.display = "none";
+        var img = document.getElementById("enlarge-img");
+        img.src = Flask.url_for('static', {'filename': reconstruct});
+    }
+    else if (isVideo(video_ext)) {
+        img_div.style.display = "none";
+        var video = document.getElementById("video-src");
+        video.src = Flask.url_for('static', {'filename': reconstruct});
+    }
 }
 
 
-img_container.onclick = function() {
-    if (img_container.style.display === "block") {
-        expandImg(this);
+media_container.onclick = function(event) {
+    // console.log(event.target.id, media_container.style.display);
+    if ((event.target.id === "c-video-display" || event.target.id === "enlarge-img") && media_container.style.display === "block") {
+        viewer(this);
     }
+};
+
+var video = document.getElementById('play-pause-masonary-vid');
+var startStop_btn = document.getElementById('play-pause');
+var video_containter = document.getElementById('video-container');
+
+function togglePlayPause() {
+    if(video.paused) {
+        startStop_btn.className = 'pause';
+        video.play();
+    }
+    else {
+        startStop_btn.className = 'play';
+        video.pause();
+    }
+}
+
+video_containter.onclick = function() {
+    togglePlayPause();
 };
