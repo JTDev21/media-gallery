d4497414f1b3a256248bbc21f42f29fdedb08432
srirachaBoi <62912895+srirachaBoi@users.noreply.github.com>
2020-08-21 11:14:08 -0600

    Introduced Infinite scrolling and lazy loading

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify, make_response
+import random
+import time
+
 from flask_jsglue import JSGlue
 import os
 
 
 
-video_path = []
+yt_db = []
+counter = 0
 for root, dirs, files in os.walk('static/videos'):
     for file in files:
-        video_path.append("videos/" + file)
+        title = file.split('.mp4')[0]
+        yt_db.append(["videos/" + file, str(counter) + " | " + title, "Joshua Truong"])
+        counter += 1
+
+
 
 media_filepaths = []
 media_ext_type = []
@@ -25,9 +33,6 @@ for root, dirs, files in os.walk('static/media_files'):
             if (media_ext == ext):
                 media_ext_type.append('image')
 
-# print(media_filepaths)
-# print("media_filepaths", len(media_filepaths))
-# print("media_ext_type", len(media_ext_type))
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -42,11 +47,42 @@ def home():
 
 @app.route("/playlist")
 def videoPlaylist():
-    return render_template("youtubeVideos.html", videoPaths=video_path)
+    return render_template("youtubeVideos.html")
 
 @app.route("/masonaryGallery.html")
 def photoGallery():
     return render_template("masonaryGallery.html", jsglue=_jsglue, mediaFiles=media_filepaths, mediaEXT=media_ext_type)
+
+# ---------------------------------------------------------------------------------------------------------------------
+posts = len(yt_db)  # num posts to generate
+quantity = 15  # num posts to return per request
+
+
+@app.route("/load-yt")
+def load_yt_items():
+    """ Route to return the posts """
+
+    time.sleep(0.2)  # Used to simulate delay
+
+    if request.args:
+        counter = int(request.args.get("c"))  # The 'counter' value sent in the QS
+
+        if counter == 0:
+            print(f"Returning posts 0 to {quantity}")
+            # Slice 0 -> quantity from the db
+            res = make_response(jsonify(yt_db[0: quantity]), 200)
+
+        elif counter == posts:
+            print("No more posts")
+            res = make_response(jsonify({}), 200)
+
+        else:
+            print(f"Returning posts {counter} to {counter + quantity}")
+            # Slice counter -> quantity from the db
+            res = make_response(jsonify(yt_db[counter: counter + quantity]), 200)
+
+    return res
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
---
+++ static/js/loadingYT-videos.js
@@ -0,0 +1,56 @@
+// Get references to the dom elements
+var scroller = document.querySelector("#scroller");
+var template = document.querySelector('#post_template');
+var sentinel = document.querySelector('#sentinel');
+var counter = 0;
+
+// Function to request new items and render to the dom
+function loadItems() {
+    console.log('loading');
+    // Use fetch to request data and pass the counter value in the QS
+    fetch(`/load-yt?c=${counter}`).then((response) => {
+
+    // Convert the response data to JSON
+    response.json().then((data) => {
+
+      // If empty JSON, exit the function
+    if (!data.length) {
+        // Replace the spinner with "No more posts"
+        sentinel.innerHTML = "No more posts";
+        return;
+    }
+
+      // Iterate over the items in the response
+    for (var i = 0; i < data.length; i++) {
+        // Clone the HTML template
+        let template_clone = template.content.cloneNode(true);
+
+        // Query & update the template content
+        console.log(data[i][1], data[i][2]);
+        template_clone.querySelector("#playlist-video-title").innerHTML = data[i][1];
+        template_clone.querySelector("#playlist-author").innerHTML = data[i][2];
+
+        // Append template to dom
+        scroller.appendChild(template_clone);
+        counter++;
+    }
+    
+    })
+    })
+}
+
+// Create a new IntersectionObserver instance
+var intersectionObserver = new IntersectionObserver(entries => {
+    // If intersectionRatio is 0, the sentinel is out of view
+    // and we don't need to do anything. Exit the function
+    if (entries[0].intersectionRatio <= 0) {
+    return;
+    }
+
+    // Call the loadItems function
+    loadItems();
+
+});
+
+// Instruct the IntersectionObserver to watch the sentinel
+intersectionObserver.observe(sentinel);
